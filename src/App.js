15c03545379b8b7e3184313df2517c34312e93e5
p-adams <ghusiyy@gmail.com>
2021-04-11 11:39:26 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.css";
+
+const position = [35.5485, -121.9886];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Qiblah Finder 🕋</h3>
       </header>
+      <main>
+        <MapContainer
+          center={position}
+          zoom={1}
+          scrollWheelZoom={false}
+          style={{ height: "400px", width: "525px" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Polyline
+            positions={[
+              [35.5485, -121.9886],
+              [21.422487, 39.826206],
+            ]}
+          />
+        </MapContainer>
+      </main>
+      <footer />
     </div>
   );
 }
