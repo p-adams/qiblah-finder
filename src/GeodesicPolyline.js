581450b7dea712c0a7f876bcb92de483203119e4
p-adams <ghusiyy@gmail.com>
2021-04-11 22:06:01 -0700

    update origin lat long

--- src/GeodesicPolyline.js
+++ src/GeodesicPolyline.js
@@ -4,7 +4,7 @@ import { GeodesicLine as LeafletGeodesicLine } from "leaflet.geodesic";
 
 function GeodesicPolyline({
   positions = [
-    [35.5485, -121.9886],
+    [37.548271, -121.988571],
     [21.422487, 39.826206],
   ],
 }) {
